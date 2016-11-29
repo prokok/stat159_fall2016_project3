dd02a0ee2088ad9e2cfca9b4905ca31f466c166e
jdelorimier <delorimier16@gmail.com>
2016-11-29 13:23:30 -0800

    script creation: provides functions for ranking schools

--- code/scripts/analysis.R
+++ code/scripts/analysis.R
@@ -1,6 +1,5 @@
-# Loading full Data File
-full_data = read.csv("../../../MERGED2014_15_PP.csv")
-
+# Loading Data FIles
+aux_data = read.csv("../../data/aux-data.csv")
 subset_dat = read.csv("../../data/subset-data.csv")
 
 #OLS rankings 
@@ -9,7 +8,7 @@ ols_hisp = read.csv("../../data/ranked-ols-hisp.csv")
 
 #Ridge Rankings
 ridge_black = read.csv("../../data/ranked-ridge-black.csv")
-rige_hisp = read.csv("../../data/ranked-ridge-hispanic.csv")
+ridge_hisp = read.csv("../../data/ranked-ridge-hispanic.csv")
 
 
 # Status is 
@@ -19,6 +18,7 @@ highly_selective = c(14,15)
 
 # State is 
 CA = 6
+NY = 36
 
 
 #####
@@ -29,17 +29,21 @@ CA = 6
 by_state = function(group, state, status) {
   four_year = which(as.numeric(subset_dat$CCUGPROF) == status[1] 
                     | as.numeric(subset_dat$CCUGPROF) == status[2])
-  st = which(full_data$ST_FIPS == state)
+  # added with aux
+  st = which(aux_data$states == state)
   four_year_st = intersect(four_year, st)
   four_year_st = intersect(group$index, four_year_st)
   temp_dat = subset(group, subset = index %in% four_year_st[1:10] & resid > 0)
-  names = full_data$INSTNM[temp_dat$index]
-  cbind(names, temp_dat)
+  # need to add
+  names = aux_data$school_name[temp_dat$index]
+  rows = temp_dat$index
+  # need to add
+  total_grad = aux_data$total_grad[rows]
+  cbind(names, subset_dat[rows,], total_grad)
 
 }
 
-
-
+by_state(ridge_hisp, CA, selective)
 
 
 
